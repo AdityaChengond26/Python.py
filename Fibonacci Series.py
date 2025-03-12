40cc5ab6ff5123abdcdb5b97cf8c07d5bfc6e315
AdityaChengond26 <adityachengond121@gmail.com>
2025-03-12 18:14:17 +0530

    Update Fibonacci Series.py

--- Fibonacci Series.py
+++ Fibonacci Series.py
@@ -1,11 +1,5 @@
-def fib(n):
-  a=0
-  b=1
-  if n==1:
-    print("0")
-  else:
-      print(a)
-      print(b)
-  for i in range(2,n):
-     a,b=b,a+b
-     print(b)
+n=int(input("enter number of terms:"))
+a,b=0,1
+for i in range(n):
+  print(a,end=" ")
+  a,b=b,a+b
